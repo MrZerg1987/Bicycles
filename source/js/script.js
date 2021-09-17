4b7c8f2b5e9a696396a72ed38c92f33db4a170a3
Kozin Sergey <6303420@mail.ru>
2021-09-17 21:32:57 +0300

    Адаптив шапки и главного блока

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,13 @@
-'use strict';
-var pageHeader = document.querySelector('.page-header');
-var headerToggle = document.querySelector('.page-header__toggle');
+// Mobile-menu
 
-pageHeader.classList.remove('page-header--nojs');
+const navToggle = document.querySelector(".main-nav__toggle");
+const navList = document.querySelector(".main-nav__list");
 
-headerToggle.addEventListener('click', function () {
-  if (pageHeader.classList.contains('page-header--closed')) {
-    pageHeader.classList.remove('page-header--closed');
-    pageHeader.classList.add('page-header--opened');
-  } else {
-    pageHeader.classList.add('page-header--closed');
-    pageHeader.classList.remove('page-header--opened');
-  }
+navList.classList.add("main-nav__list--close");
+navToggle.classList.remove("main-nav__toggle--no-js");
+navList.classList.remove("main-nav__list--no-js");
+
+navToggle.addEventListener("click", function () {
+  navList.classList.toggle("main-nav__list--close");
+  navToggle.classList.toggle("main-nav__toggle--close");
 });
