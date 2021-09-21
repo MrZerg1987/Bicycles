00e6a25ce27cf74231d7234dafaeabe8bbfd6075
Kozin Sergey <6303420@mail.ru>
2021-09-21 07:17:15 +0300

    Правки по поводу переполнения. Первые три блока

--- build/js/script.js
+++ build/js/script.js
@@ -1,13 +1,13 @@
-// Mobile-menu
+// // Mobile-menu
 
-const navToggle = document.querySelector(".main-nav__toggle");
-const navList = document.querySelector(".main-nav__list");
+// const navToggle = document.querySelector(".main-nav__toggle");
+// const navList = document.querySelector(".main-nav__list");
 
-navList.classList.add("main-nav__list--close");
-navToggle.classList.remove("main-nav__toggle--no-js");
-navList.classList.remove("main-nav__list--no-js");
+// navList.classList.add("main-nav__list--close");
+// navToggle.classList.remove("main-nav__toggle--no-js");
+// navList.classList.remove("main-nav__list--no-js");
 
-navToggle.addEventListener("click", function () {
-  navList.classList.toggle("main-nav__list--close");
-  navToggle.classList.toggle("main-nav__toggle--close");
-});
+// navToggle.addEventListener("click", function () {
+//   navList.classList.toggle("main-nav__list--close");
+//   navToggle.classList.toggle("main-nav__toggle--close");
+// });
